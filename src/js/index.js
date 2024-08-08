ef219510c85a27afb4927fbb0ba0b6e47c5a458e
池田 遼 <ryouslash@gmail.com>
2024-08-08 19:49:55 +0400

    お問い合わせセクション 作成中

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 
 import { config, dom, library } from '@fortawesome/fontawesome-svg-core';
-import {faPenNib, faCode, faServer,faChevronDown} from '@fortawesome/free-solid-svg-icons';
-import {faEnvelope, faHandshake} from '@fortawesome/free-regular-svg-icons';
+import {faPenNib, faCode, faServer,faChevronDown, faLink} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHandshake } from '@fortawesome/free-regular-svg-icons';
+import {faInstagram, faFacebook} from '@fortawesome/free-brands-svg-icons';
 
-library.add(faEnvelope, faHandshake, faPenNib, faCode, faServer,faChevronDown);
+
+library.add(faEnvelope, faHandshake, faPenNib, faCode, faServer,faChevronDown, faLink,faInstagram, faFacebook);
 
 dom.i2svg();
