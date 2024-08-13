a438e8947f255935211f75e3a8653fc69ecfe7e9
池田 遼 <ryouslash@gmail.com>
2024-08-13 18:07:53 +0400

    固定ページ用テンプレートの作成

--- dist/js/main.js
+++ dist/js/main.js
@@ -30426,8 +30426,26 @@ function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length)
 
 
 
+
+// Font awesome 読み込み
 _fortawesome_fontawesome_svg_core__WEBPACK_IMPORTED_MODULE_2__.library.add(_fortawesome_free_regular_svg_icons__WEBPACK_IMPORTED_MODULE_3__.faEnvelope, _fortawesome_free_regular_svg_icons__WEBPACK_IMPORTED_MODULE_3__.faHandshake, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faPenNib, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faCode, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faServer, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faChevronDown, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faLink, _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faInstagram, _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faFacebook, _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faGithub, _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_5__.faLinkedin, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faChevronUp, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faBars, _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_4__.faXmark);
 _fortawesome_fontawesome_svg_core__WEBPACK_IMPORTED_MODULE_2__.dom.i2svg();
+
+// 100vwの調整
+function setVw() {
+  // --vwをセットする関数
+  var vw = jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).width() / 100 + "px";
+  // ブラウザ幅/100を取得し変数vwに格納
+  jquery__WEBPACK_IMPORTED_MODULE_0___default()(":root").css("--vw", vw);
+  // :rootのカスタムプロパティ--vwにvwを代入させる。これで、スクロールバーの幅を除いた画面幅/100が--vwになる
+}
+jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).on("load resize", function () {
+  setVw();
+});
+// 画面を、読み込んだ時・サイズを変えた時  →  関数setVwが動作する
+
+// 初期ロード時にも関数を実行
+setVw();
 var header = document.querySelector('.l-header');
 var headerHeight = header.offsetHeight; // 数値として保持
 
--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,27 @@ import {faPenNib, faCode, faServer,faChevronDown, faLink, faChevronUp, faBars, f
 import { faEnvelope, faHandshake } from '@fortawesome/free-regular-svg-icons';
 import {faInstagram, faFacebook, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
-
+// Font awesome 読み込み
 library.add(faEnvelope, faHandshake, faPenNib, faCode, faServer,faChevronDown, faLink,faInstagram, faFacebook, faGithub, faLinkedin, faChevronUp, faBars, faXmark);
 
 dom.i2svg();
+
+// 100vwの調整
+function setVw() {
+  // --vwをセットする関数
+  let vw = $(window).width() / 100 + "px";
+  // ブラウザ幅/100を取得し変数vwに格納
+  $(":root").css("--vw", vw);
+  // :rootのカスタムプロパティ--vwにvwを代入させる。これで、スクロールバーの幅を除いた画面幅/100が--vwになる
+}
+
+$(window).on("load resize", function () {
+  setVw();
+});
+// 画面を、読み込んだ時・サイズを変えた時  →  関数setVwが動作する
+
+// 初期ロード時にも関数を実行
+setVw();
 
 const header = document.querySelector('.l-header');
 const headerHeight = header.offsetHeight; // 数値として保持
