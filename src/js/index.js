84f8fc8fcfbfdc2157984b618744077d45660af1
池田 遼 <ryouslash@gmail.com>
2024-08-13 20:02:49 +0400

    固定ページ、投稿ページのテンプレート作成中

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,12 @@ import $ from 'jquery';
 import 'slick-carousel/slick/slick.min.js';
 
 import { config, dom, library } from '@fortawesome/fontawesome-svg-core';
-import {faPenNib, faCode, faServer,faChevronDown, faLink, faChevronUp, faBars, faXmark} from '@fortawesome/free-solid-svg-icons';
+import {faPenNib, faCode, faServer,faChevronDown, faLink, faChevronUp, faBars, faXmark, faCheck} from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope, faHandshake } from '@fortawesome/free-regular-svg-icons';
 import {faInstagram, faFacebook, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 // Font awesome 読み込み
-library.add(faEnvelope, faHandshake, faPenNib, faCode, faServer,faChevronDown, faLink,faInstagram, faFacebook, faGithub, faLinkedin, faChevronUp, faBars, faXmark);
+library.add(faEnvelope, faHandshake, faPenNib, faCode, faServer,faChevronDown, faLink,faInstagram, faFacebook, faGithub, faLinkedin, faChevronUp, faBars, faXmark, faCheck);
 
 dom.i2svg();
 
