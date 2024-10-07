0bea2b622e20e04b38089e278ba122370cf86f83
池田 遼 <ryouslash@gmail.com>
2024-10-07 18:57:59 +0400

    記事が1つしかない場合に、前後リンク全体を表示しないように変更

--- template-parts/prevnextnavi.php
+++ template-parts/prevnextnavi.php
@@ -1,18 +1,22 @@
-<div class="c-prevNextNavi">
-  <div class="c-prevNextNavi__item c-prevNextNavi__prev">
+<?php
+$previous_post = get_previous_post();
+$next_post = get_next_post();
 
-    <?php
-    $previous_post = get_previous_post();
-    if ($previous_post): ?>
-      <a href="<?php echo esc_url(get_permalink($previous_post)); ?>"><i class="fa-solid fa-chevron-left"></i> 前の投稿へ</a>
-    <?php endif; ?>
-  </div>
+// 前後の投稿がどちらも存在しない場合はナビゲーション全体を表示しない
+if ($previous_post || $next_post): ?>
+  <div class="c-prevNextNavi">
+    <div class="c-prevNextNavi__item c-prevNextNavi__prev">
+      <?php
+      if ($previous_post): ?>
+        <a href="<?php echo esc_url(get_permalink($previous_post)); ?>"><i class="fa-solid fa-chevron-left"></i> 前の投稿へ</a>
+      <?php endif; ?>
+    </div>
 
-  <div class="c-prevNextNavi__item c-prevNextNavi__next">
-    <?php
-    $next_post = get_next_post();
-    if ($next_post): ?>
-      <a href="<?php echo (get_permalink($next_post)); ?>">次の投稿へ <i class="fa-solid fa-chevron-right"></i></a>
-    <?php endif; ?>
+    <div class="c-prevNextNavi__item c-prevNextNavi__next">
+      <?php
+      if ($next_post): ?>
+        <a href="<?php echo (get_permalink($next_post)); ?>">次の投稿へ <i class="fa-solid fa-chevron-right"></i></a>
+      <?php endif; ?>
+    </div>
   </div>
-</div>
+<?php endif; ?>
