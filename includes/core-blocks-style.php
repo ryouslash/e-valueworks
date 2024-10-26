5a3967071ae5282f932e25f8b3e1927585190cdd
池田 遼 <ryouslash@gmail.com>
2024-10-26 14:20:41 +0400

    引用箇所のスタイル作成、リストブロックに下線付きリストのスタイル追加

--- includes/core-blocks-style.php
+++ includes/core-blocks-style.php
@@ -10,5 +10,13 @@ function custom_block_styles()
       'label' => 'シャドウ', // スタイルの表示名
     )
   );
+
+  register_block_style(
+    'core/list',
+    array(
+      'name'  => 'borderBottom', // スタイル名
+      'label' => '下線付き', // スタイルの表示名
+    )
+  );
 }
 add_action('init', 'custom_block_styles');
